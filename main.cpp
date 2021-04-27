d2deb2338c51c9a37412666719efadee58677667
Piotr Drabik <UL0246725@edu.uni.lodz.pl>
2021-04-27 14:53:23 +0200

    added two classes this should be sufficient to resolve the task

--- main.cpp
+++ main.cpp
@@ -2,11 +2,33 @@
 // Created by pc on 27.04.2021.
 //
 #include <iostream>
+#include <vector>
 
 
-int main(){
+struct change {
+    change() : rate_(1), from_(0), to_(0) {};
 
-    std::cout<<"hi";
+    change(unsigned int from, unsigned int to, unsigned int rate) : from_(from), to_(to), rate_(rate) {};
+    unsigned rate_;
+    unsigned from_;
+    unsigned to_;
+
+};
+
+struct node {
+    node(unsigned value) : value_(value) {};
+
+    void add_connection(change new_route) {
+        list_of_connections_.push_back(new_route); }
+
+    unsigned value_;
+    std::vector<change> list_of_connections_;
+};
+
+
+int main() {
+
+    std::cout << "hi";
 
 
 
